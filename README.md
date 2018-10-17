266b35245eda0a40161fa04ff184cee16ade527f
jason <jason@jasondeMac.local>
2018-10-17 00:40:44 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WebPractice
+## Author:史亚运
+## Emain:284469828@qq.com
 Go application practice
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome You~")
+}
+
+func main() {
+	//	http.Server = {
+	//		Addr: ":8000"
+	//	}
+	http.HandleFunc("/", Home)
+	http.ListenAndServe(":8000", nil)
+	fmt.Println("Hello Web")
+}
